80c09a6d7d4ec22d7ce94548c4b896d67edbb598
DESKTOP-1B0NP9S\17292 <wangzhechao21@mails.ucas.ac.cn>
2022-08-23 17:49:20 +0800

    no message

---
+++ mypath.py
@@ -0,0 +1,13 @@
+class Path(object):
+    @staticmethod
+    def db_root_dir(dataset):
+
+        if dataset == 'pascal':
+            return 'your path to pascal dataset'  # folder that contains VOCdevkit/.
+        elif dataset == 'sbd':
+            return 'your path to sbd dataset'  # folder that contains dataset/.
+        elif dataset == 'cityscapes':
+            return 'D:\PycharmProjects\Auto-Dynamic-DeepLab-master\Cityscapes' # your path to cityscapes dataset'     # foler that contains leftImg8bit/
+        else:
+            print('Dataset {} not available.'.format(dataset))
+            raise NotImplementedError
